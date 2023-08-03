cc18abf32dfd47c90f9a85122e3dbc362a63e196
I-Hsi Kao <i-hsi.kao@upower.com>
2023-08-03 12:34:54 -0700

    change LICENSE

--- rmw_fastrtps_shared_cpp/include/fabric_rmw/fabric_functions.hpp
+++ rmw_fastrtps_shared_cpp/include/fabric_rmw/fabric_functions.hpp
@@ -1,4 +1,4 @@
-// Copyright 2019 ADLINK Technology via Rover Robotics and Dan Rose
+// Copyright 2023 U Power Robotics USA, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
